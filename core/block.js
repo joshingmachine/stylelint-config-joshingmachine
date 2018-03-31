7dbf0f32aa2d8b970f3a9d175182635ba89a2dd0
joshingmachine <josh@joshingmachine.com>
2018-03-30 23:53:22 -0400

    Fixed block and general linting errors

--- core/block.js
+++ core/block.js
@@ -1,31 +1,34 @@
+const possibleErrors = {
+    'block-no-empty': true,
+}
+
+const stylisticIssues = {
+    'block-closing-brace-empty-line-before': [
+        'never',
+        {
+            except: [],
+        },
+    ],
+    'block-closing-brace-newline-after': [
+        'always',
+        {
+            ignoreAtRules: [],
+        },
+    ],
+    'block-closing-brace-newline-before': 'always',
+    'block-closing-brace-space-after': 'never',
+    'block-closing-brace-space-before': 'always-single-line',
+    'block-opening-brace-newline-after': 'always',
+    'block-opening-brace-newline-before': null,
+    'block-opening-brace-space-after': 'always-single-line',
+    'block-opening-brace-space-before': [
+        'always',
+        {
+            ignoreAtRules: [],
+        },
+    ],
+}
+
 module.exports = {
-    rules: {
-        // Possible errors
-        'block-no-empty': true,
-        // Stylistic issues
-        'block-closing-brace-empty-line-before': [
-            'never',
-            {
-                except: [],
-            },
-        ],
-        'block-closing-brace-newline-after': [
-            'always',
-            {
-                ignoreAtRules: [],
-            },
-        ],
-        'block-closing-brace-newline-before': 'always',
-        'block-closing-brace-space-after': 'never',
-        'block-closing-brace-space-before': 'always-single-line',
-        'block-opening-brace-newline-after': 'always',
-        'block-opening-brace-newline-before': null,
-        'block-opening-brace-space-after': 'always-single-line',
-        'block-opening-brace-space-before': [
-            'always',
-            {
-                ignoreAtRules: [],
-            },
-        ],
-    },
+    rules: Object.assign({}, possibleErrors, stylisticIssues),
 }
--- core/general.js
+++ core/general.js
@@ -1,11 +1,15 @@
+const SPACES_PER_INDENT = 4
+
 module.exports = {
     rules: {
-        // Stylistic issues
-        indentation: [ 4, {
-            indentInsideParens: null,
-            indentClosingBrace: false,
-            except: [],
-            ignore: [[,
-        }],
+        indentation: [
+            SPACES_PER_INDENT,
+            {
+                except: [],
+                ignore: [],
+                indentClosingBrace: false,
+                indentInsideParens: null,
+            },
+        ],
     },
 }
