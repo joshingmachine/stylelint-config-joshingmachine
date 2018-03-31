6fc62da0be6f16f9a3292fb755cb6056daf81162
joshingmachine <josh@joshingmachine.com>
2018-03-30 23:21:28 -0400

    Extend rule configs in core index

---
+++ core/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: [
+        './core/index.js',
+        './at-rule.js',
+        './block.js',
+        './color.js',
+        './font-family.js',
+        './function.js',
+        './general.js',
+    ],
+}
