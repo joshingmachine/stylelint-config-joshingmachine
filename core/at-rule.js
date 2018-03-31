999d8d4f91ccec0a7639010d49bee58f95bad345
joshingmachine <josh@joshingmachine.com>
2018-03-30 23:38:55 -0400

    Fixing remaining linting errors manually

--- core/at-rule.js
+++ core/at-rule.js
@@ -1,29 +1,39 @@
+const possibleErrors = {
+    'at-rule-no-unknown': [
+        true,
+        {
+            ignoreAtRules: [],
+        },
+    ],
+}
+
+const limitLanguageFeatures = {
+    'at-rule-blacklist': null,
+    'at-rule-no-vendor-prefix': true,
+    'at-rule-whitelist': null,
+}
+
+const stylisticIssues = {
+    'at-rule-empty-line-before': [
+        'always',
+        {
+            except: ['first-nested'],
+            ignore: ['after-comment'],
+            ignoreAtRules: [],
+        },
+    ],
+    'at-rule-name-case': 'lower',
+    'at-rule-name-newline-after': 'always-multi-line',
+    'at-rule-name-space-after': 'always',
+    'at-rule-semicolon-newline-after': 'always',
+    'at-rule-semicolon-space-before': 'never',
+}
+
 module.exports = {
-    rules: {
-        // Possible errors
-        'at-rule-no-unknown': [
-            true,
-            {
-                ignoreAtRules: [],
-            },
-        ],
-        // Limit language features
-        'at-rule-blacklist': null,
-        'at-rule-no-vendor-prefix': true,
-        'at-rule-whitelist': null,
-        // Stylistic issues
-        'at-rule-empty-line-before': [
-            'always',
-            {
-                except: ['first-nested'],
-                ignore: ['after-comment'],
-                ignoreAtRules: [],
-            },
-        ],
-        'at-rule-name-case': 'lower',
-        'at-rule-name-newline-after': 'always-multi-line',
-        'at-rule-name-space-after': 'always',
-        'at-rule-semicolon-newline-after': 'always',
-        'at-rule-semicolon-space-before': 'never',
-    },
+    rules: Object.assign(
+        {},
+        possibleErrors,
+        limitLanguageFeatures,
+        stylisticIssues
+    ),
 }
--- core/font-family.js
+++ core/font-family.js
@@ -1,9 +1,12 @@
 module.exports = {
     rules: {
         // Possible errors
-        'font-family-no-duplicate-names': [ true, {
-            ignoreFontFamilyNames: [],
-        },
+        'font-family-no-duplicate-names': [
+            true,
+            {
+                ignoreFontFamilyNames: [],
+            },
+        ],
         'font-family-no-missing-generic-family-keyword': true,
         // Stylistic issues
         'font-family-name-quotes': 'always-where-recommended',
